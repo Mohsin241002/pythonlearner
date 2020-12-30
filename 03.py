4b57ef3585f2f4bac53d16baf8c9fda21d5f2463
Mohsin <chunawala246@gmaile.com>
2020-12-31 03:39:37 +0530

    fdg

--- 03.py
+++ 03.py
@@ -1,13 +1,13 @@
-# a = 43
-# b = 34
-# print("a + b =", a+b)
+a = 43
+b = 34
+print("a + b =", a+b)
 
-# a = input("enter first number  ")
-# b = input("enter second number  ")
-# a = int(a)
-# b = int(b)
-# print((a+b)/2)
+a = input("enter first number  ")
+b = input("enter second number  ")
+a = int(a)
+b = int(b)
+print((a+b)/2)
 
-# a = input("enter a number  ")
-# a = int(a)
-# print(a*a)
+a = input("enter a number  ")
+a = int(a)
+print(a*a)
